baf4dae05d318741a905bb50547545baa40fc208
edouardlemessi <towabalo.ndjada@gmail.com>
2021-01-04 00:26:08 +0100

    first

---
+++ src/main/java/com/tomgregory/ThemeParkApplication.java
@@ -0,0 +1,29 @@
+package com.tomgregory;
+
+import com.tomgregory.entity.ThemeParkRide;
+import com.tomgregory.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.Collections;
+
+@SpringBootApplication
+public class
+ThemeParkApplication  {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ThemeParkApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+    }
+
+    @Bean
+    public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+        return (args) -> {
+            repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+            repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+            repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+        };
+    }
+}
